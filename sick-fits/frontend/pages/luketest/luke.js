18882a4de50703ecad3219742ecef50e1b111b3a
lchamberlain <lchamerlain@klipfolio.com>
2019-02-22 13:57:43 -0500

    Progress

---
+++ sick-fits/frontend/pages/luketest/luke.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+// class Home extends React.Component {
+//     render(){
+//         return <p>Hey Luke</p>
+//     }
+// }
+
+const Home = (props) => (
+    <div>
+        <p>Hey Luke!</p>
+    </div>
+);
+
+export default Home;
